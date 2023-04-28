9c357f914304a703ce3170c32d83a448999c65c6
Eric Yanalis <ericyanalis@gmail.com>
2023-04-27 20:08:07 -0400

    first commit

---
+++ lua/darkrp_customthings/jobs.lua
@@ -0,0 +1,64 @@
+--[[---------------------------------------------------------------------------
+DarkRP custom jobs
+---------------------------------------------------------------------------
+This file contains your custom jobs.
+This file should also contain jobs from DarkRP that you edited.
+
+Note: If you want to edit a default DarkRP job, first disable it in darkrp_config/disabled_defaults.lua
+      Once you've done that, copy and paste the job to this file and edit it.
+
+The default jobs can be found here:
+https://github.com/FPtje/DarkRP/blob/master/gamemode/config/jobrelated.lua
+
+For examples and explanation please visit this wiki page:
+https://darkrp.miraheze.org/wiki/DarkRP:CustomJobFields
+
+Add your custom jobs under the following line:
+---------------------------------------------------------------------------]]
+
+TEAM_THIEF = DarkRP.createJob("Thief", {
+    color = Color(75, 75, 75, 255),
+    model = "models/player/css/t_arctic.mdl",
+    description = [[A thief]],
+    weapons = {"lockpick"},
+    command = "thief",
+    max = 2,
+    salary = GAMEMODE.Config.normalsalary,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    category = "Gangsters",
+})
+
+TEAM_GUN = DarkRP.createJob("Gun Dealer2", {
+    color = Color(255, 140, 0, 255),
+    model = "models/Humans/Group03/Male_05.mdl",
+    description = [[A Gun Dealer is the only person who can sell guns to other people.
+        Make sure you aren't caught selling illegal firearms to the public! You might get arrested!]],
+    weapons = {},
+    command = "gundealer2",
+    max = 2,
+    salary = GAMEMODE.Config.normalsalary,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    category = "Citizens",
+})
+
+--[[---------------------------------------------------------------------------
+Define which team joining players spawn into and what team you change to if demoted
+---------------------------------------------------------------------------]]
+GAMEMODE.DefaultTeam = TEAM_CITIZEN
+--[[---------------------------------------------------------------------------
+Define which teams belong to civil protection
+Civil protection can set warrants, make people wanted and do some other police related things
+---------------------------------------------------------------------------]]
+GAMEMODE.CivilProtection = {
+    [TEAM_POLICE] = true,
+    [TEAM_CHIEF] = true,
+    [TEAM_MAYOR] = true,
+}
+--[[---------------------------------------------------------------------------
+Jobs that are hitmen (enables the hitman menu)
+---------------------------------------------------------------------------]]
+DarkRP.addHitmanTeam(TEAM_MOB)
